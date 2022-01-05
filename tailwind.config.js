06e1416d7a3dc91eb4ee1f3134bfff2c94c6f802
Brandon Charest <b.charest019@gmail.com>
2022-01-05 15:35:11 -0500

    header bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
