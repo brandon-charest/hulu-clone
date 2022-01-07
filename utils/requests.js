1d0961c9a9c58fb147b4408ee0ccdae2a01ab5c9
Brandon Charest <b.charest019@gmail.com>
2022-01-07 15:48:19 -0500

    fixed copy/paste error

--- utils/requests.js
+++ utils/requests.js
@@ -7,43 +7,43 @@ export default {
   },
   fetchTopRated: {
     title: 'Top Rated',
-    url: `/trending/top_rated/week?api_key=${API_KEY}&language=en-US`
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
   },
   fetchAction: {
     title: 'Action',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=28`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`
   },
   fetchAdventure: {
     title: 'Adventure',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=12`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=12`
   },
   fetchAnimation: {
     title: 'Animation',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=16`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=16`
   },
   fetchComedy: {
     title: 'Comedy',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=35`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`
   },
   fetchCrime: {
     title: 'Crime',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=80`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=80`
   },
   fetchDocumentary: {
     title: 'Documentary',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=99`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=99`
   },
   fetchDrama: {
     title: 'Drama',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=18`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=18`
   },
   fetchFamily: {
     title: 'Family',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=10751`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10751`
   },
   fetchFantasy: {
     title: 'Fantasy',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=14`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=14`
   },
   fetchHistory: {
     title: 'History',
@@ -51,34 +51,34 @@ export default {
   },
   fetcHorror: {
     title: 'Horror',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=27`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`
   },
   fetchMusic: {
     title: 'Music',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=10402`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10402`
   },
   fetchMystery: {
     title: 'Mystery',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=9648`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`
   },
   fetchRomance: {
     title: 'Romance',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=10749`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`
   },
   fetchScienceFiction: {
     title: 'Science Fiction',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=878`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=878`
   },
   fetchThriller: {
     title: 'Thriller',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=53`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=53`
   },
   fetchWar: {
     title: 'War',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=10752`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10752`
   },
   fetcWestern: {
     title: 'Western',
-    url: `/trending/movie/week?api_key=${API_KEY}&with_genres=37`
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`
   }
 }
